b56337c77bd9df04bb3716ac1cc83be83970442d
Guopeng Liang <guopeng.liang@gmail.com>
2013-03-14 01:31:57 +0800

    Fixed German date translation bug.

--- lang.de.js
+++ lang.de.js
@@ -15,8 +15,8 @@
 		},
 
 		string: {
-			"weekdays": "Montag_Dienstag_Mittwoch_Donnerstag_Freitag_Samstag_Sonntag".split("_"),
-			"weekdaysShort": "Mo_Di_Mi_Do_Fr_Sa_So".split("_"),
+			"weekdays": "Sonntag_Montag_Dienstag_Mittwoch_Donnerstag_Freitag_Samstag".split("_"),
+			"weekdaysShort": "So_Mo_Di_Mi_Do_Fr_Sa".split("_"),
 			"pullText": "Neue ..._Neue k..._Neue ko..._Neue kos..._Neue kost..._Neue koste..._Neue kosten..._Neue kosten!".split("_"),
 			// "formatMetaDate": 'MM[<span>]DD[</span>] [<span class="day">]ddd[</span>]',
 			"formatMetaDate": '[<strong>]DD[</strong>].MM',
